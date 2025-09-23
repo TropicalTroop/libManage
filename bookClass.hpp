f517536db3862d4daf6f3a6adf8c6ce92ef4bb88
atharvaMac <atharva.sonune@gmail.com>
2025-09-23 11:56:04 -0700

    documentation

--- bookClass.hpp
+++ bookClass.hpp
@@ -9,22 +9,36 @@ Atharva, Nikhil, Po
 // Class Definition for Book
 class Book{
 public:
-   Book();
+   Book(); //Default Constructor
+   
    Book(std::string, std::string, int, std::string, bool, int);
+   //Takes in Title, Author, ISBN, genre, Availability, and barcode into overloaded constructor.
+   
    std::string getTitle() const;
+   //Returns the book's title as std::string.
+   
    std::string getAuthor() const;
+   //returns book's author as std::string.
+   
    std::string getGenre() const;
+   //returns book's genre as std::string.
+   
    bool getAvailability() const;
+   //returns book's availability as true or false bool.
+   
    int getBarcode() const;
+   //returns the book's barcode as an integer
+   
    int getISBN() const;
+   //returns book's ISBN as an integer.
    
 private:
-   std::string title;
-   std::string author;
-   int ISBN;
-   std::string genre;
-   bool availability;
-   int barcode;
+   std::string title; //Stores the title as std::string
+   std::string author; //stores the author as std::string
+   int ISBN; //stores ISBN as integer
+   std::string genre; //stores genre as std::string
+   bool availability; //stores availability as bool
+   int barcode; //stores barcode as integer
 };
 
 /*
