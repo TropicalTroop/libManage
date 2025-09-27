3c19a70a23527430cca7bdd2c17232a030930a29
atharvaMac <atharva.sonune@gmail.com>
2025-09-26 18:40:36 -0700

    updated user class - nikhil

--- user.h
+++ user.h
@@ -1,20 +1,37 @@
-#include "book_class.hpp"
-#include "library_class.hpp"
+// Nikhil Aadapu, Atharva, Po
+// 25 September 2025
+// Defining class User
 
+#include "book.h"
+#include <vector>
+#include <string>
+   
 #ifndef USER_H
 #define USER_H
 
-class User{
-public:
-   User();
-   User(std::string, int);
-   
-private:
-   std::vector<Book> loans;
-   std::vector<Book> holds;
-   std::string name;
-   int age;
-   double overdraft;
+class User
+{
+    public:
+        // Overloaded constructor which is called when object is initialized with parameters of type string
+        // and type int respectively
+        // @param n name of user
+        // @param a age of user
+        User(std::string n, int a);
+
+        // Adds a book pointer to the user's borrowed books list
+        // @param book pointer to a Book the user is borrowing
+        // @return void
+        void addBook(Book* book);
+
+        // Removes a book pointer from the user's borrowed books list
+        // @param book pointer to the Book to be returned
+        // @return void
+        void removeBook(Book* book);
+
+    private:
+        std::vector<Book*> myBooks;  // list of books user has borrowed
+        std::string name;            // name of user
+        int age;                     // age of user
 };
 
 #endif
